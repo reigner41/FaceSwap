3d6eaa8b1bbb00bbfb73bfd1863882489a15b703
Reigner Ouano <60680229+reigner41@users.noreply.github.com>
2023-10-15 20:41:41 +0800

    Initial Commit

---
+++ faceswap.py
@@ -0,0 +1,150 @@
+import cv2
+import dlib
+import numpy as np
+
+def extract_index_nparray(nparray):
+    """Extract index from a numpy array."""
+    index = None
+    for num in nparray[0]:
+        index = num
+        break
+    return index
+
+# Load images
+img = cv2.imread("/Users/reignerouano/DeepFake/Didi.jpeg")
+# Convert the image to grayscale
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Create a mask with the same dimensions as the grayscale image
+mask = np.zeros_like(img_gray)
+# Load the second image
+img2 = cv2.imread("/Users/reignerouano/DeepFake/leonardo.webp")
+# Convert the second image to grayscale
+img2_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+# Initialize dlib face detector and predictor
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+# Find landmarks in the first image
+faces = detector(img_gray)
+for face in faces:
+    landmarks = predictor(img_gray, face)
+    landmarks_points = []
+    for n in range(0, 68):  # There are 68 landmark points on a face
+        x = landmarks.part(n).x
+        y = landmarks.part(n).y
+        landmarks_points.append((x, y))
+
+    # Create a convex hull based on the landmarks
+    points = np.array(landmarks_points, np.int32)
+    convexhull = cv2.convexHull(points)
+    # Fill the mask with white color inside the convex hull
+    cv2.fillConvexPoly(mask, convexhull, 255)
+
+    # Triangulate the face region
+    rect = cv2.boundingRect(convexhull)
+    subdiv = cv2.Subdiv2D(rect)
+    subdiv.insert(landmarks_points)
+    triangles = subdiv.getTriangleList()
+    triangles = np.array(triangles, dtype=np.int32)
+
+    indexes_triangles = []
+    for t in triangles:
+        # Extract points of each triangle
+        pt1 = (t[0], t[1])
+        pt2 = (t[2], t[3])
+        pt3 = (t[4], t[5])
+        # Find corresponding landmark points for each vertex of the triangle
+        index_pt1 = np.where((points == pt1).all(axis=1))
+        index_pt1 = extract_index_nparray(index_pt1)
+        index_pt2 = np.where((points == pt2).all(axis=1))
+        index_pt2 = extract_index_nparray(index_pt2)
+        index_pt3 = np.where((points == pt3).all(axis=1))
+        index_pt3 = extract_index_nparray(index_pt3)
+        # Store the indices of the triangles' points
+        if index_pt1 is not None and index_pt2 is not None and index_pt3 is not None:
+            triangle = [index_pt1, index_pt2, index_pt3]
+            indexes_triangles.append(triangle)
+
+# Find landmarks in the second image
+faces2 = detector(img2_gray)
+for face in faces2:
+    landmarks2 = predictor(img2_gray, face)
+    landmarks_points2 = []
+    for n in range(0, 68):
+        x = landmarks2.part(n).x
+        y = landmarks2.part(n).y
+        landmarks_points2.append((x, y))
+
+# Face swapping process starts here
+img2_new_face = np.zeros_like(img2)
+
+# Triangulation of both faces
+for triangle_index in indexes_triangles:
+    # Triangles from the first image
+    tr1_pt1 = landmarks_points[triangle_index[0]]
+    tr1_pt2 = landmarks_points[triangle_index[1]]
+    tr1_pt3 = landmarks_points[triangle_index[2]]
+    triangle1 = np.array([tr1_pt1, tr1_pt2, tr1_pt3], np.int32)
+
+    # Crop the region of the triangle in the first image
+    rect1 = cv2.boundingRect(triangle1)
+    (x, y, w, h) = rect1
+    cropped_triangle = img[y: y + h, x: x + w]
+    cropped_tr1_mask = np.zeros((h, w), np.uint8)
+
+    # Create a mask for the cropped region of the triangle
+    points = np.array([[tr1_pt1[0] - x, tr1_pt1[1] - y],
+                       [tr1_pt2[0] - x, tr1_pt2[1] - y],
+                       [tr1_pt3[0] - x, tr1_pt3[1] - y]], np.int32)
+    cv2.fillConvexPoly(cropped_tr1_mask, points, 255)
+
+    # Triangles from the second image
+    tr2_pt1 = landmarks_points2[triangle_index[0]]
+    tr2_pt2 = landmarks_points2[triangle_index[1]]
+    tr2_pt3 = landmarks_points2[triangle_index[2]]
+    triangle2 = np.array([tr2_pt1, tr2_pt2, tr2_pt3], np.int32)
+
+    # Crop the region of the triangle in the second image
+    rect2 = cv2.boundingRect(triangle2)
+    (x, y, w, h) = rect2
+    cropped_tr2_mask = np.zeros((h, w), np.uint8)
+
+    # Create a mask for the cropped region of the triangle in the second image
+    points2 = np.array([[tr2_pt1[0] - x, tr2_pt1[1] - y],
+                        [tr2_pt2[0] - x, tr2_pt2[1] - y],
+                        [tr2_pt3[0] - x, tr2_pt3[1] - y]], np.int32)
+    cv2.fillConvexPoly(cropped_tr2_mask, points2, 255)
+
+    # Warp triangles to align the points from the first image to the second
+    points = np.float32(points)
+    points2 = np.float32(points2)
+    M = cv2.getAffineTransform(points, points2)
+    warped_triangle = cv2.warpAffine(cropped_triangle, M, (w, h))
+    # Use the mask to isolate the triangle region
+    warped_triangle = cv2.bitwise_and(warped_triangle, warped_triangle, mask=cropped_tr2_mask)
+
+    # Reconstruct the destination face with the warped triangle
+    img2_new_face_rect_area = img2_new_face[y: y + h, x: x + w]
+    img2_new_face_rect_area_gray = cv2.cvtColor(img2_new_face_rect_area, cv2.COLOR_BGR2GRAY)
+    _, mask_triangles_designed = cv2.threshold(img2_new_face_rect_area_gray, 1, 255, cv2.THRESH_BINARY_INV)
+    warped_triangle = cv2.bitwise_and(warped_triangle, warped_triangle, mask=mask_triangles_designed)
+    img2_new_face_rect_area = cv2.add(img2_new_face_rect_area, warped_triangle)
+    img2_new_face[y: y + h, x: x + w] = img2_new_face_rect_area
+
+# Combine the two faces
+img2_face_mask = np.zeros_like(img2_gray)
+img2_head_mask = cv2.fillConvexPoly(img2_face_mask, convexhull, 255)
+img2_face_mask = cv2.bitwise_not(img2_head_mask)
+
+img2_head_noface = cv2.bitwise_and(img2, img2, mask=img2_face_mask)
+result = cv2.add(img2_head_noface, img2_new_face)
+
+(x, y, w, h) = cv2.boundingRect(convexhull)
+center_face2 = (int((x + x + w) / 2), int((y + y + h) / 2))
+# Seamless cloning is used to merge the swapped face onto the original image
+seamlessclone = cv2.seamlessClone(result, img2, img2_head_mask, center_face2, cv2.NORMAL_CLONE)
+
+cv2.imshow("seamlessclone", seamlessclone)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
